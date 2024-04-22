3eae4d6355c7c4f3add30e42a6a5b0bbab99336f
JB-ID <mjbarl831@gmail.com>
2024-04-22 14:51:50 +0700

    Latihan object 1

---
+++ Latihanobject1_MJabbar/script.js
@@ -0,0 +1,349 @@
+const stokJual = [
+  {
+    kode_barang: "GL-001",
+    nama_produk: "Gulaku",
+    harga: 16.0,
+    stok: 30,
+  },
+  {
+    kode_barang: "GL-002",
+    nama_produk: "Gula curah",
+    harga: 12.0,
+    stok: 40,
+  },
+  {
+    kode_barang: "MYK-003",
+    nama_produk: "Real oil",
+    harga: 25.0,
+    stok: 20,
+  },
+  {
+    kode_barang: "TPG-004",
+    nama_produk: "Terigu",
+    harga: 10.0,
+    stok: 15,
+  },
+  {
+    kode_barang: "TPG-005",
+    nama_produk: "Aci",
+    harga: 7.0,
+    stok: 7,
+  },
+  {
+    kode_barang: "ODL-006",
+    nama_produk: "Pasta Gigi",
+    harga: 12.0,
+    stok: 20,
+  },
+  {
+    kode_barang: "MSK-007",
+    nama_produk: "Masker",
+    harga: 10.0,
+    stok: 80,
+  },
+  {
+    kode_barang: "GRM-008",
+    nama_produk: "Garam dapur",
+    harga: 9.0,
+    stok: 22,
+  },
+  {
+    kode_barang: "GRM-009",
+    nama_produk: "Garam krosok",
+    harga: 8.0,
+    stok: 50,
+  },
+  {
+    kode_barang: "MIE-010",
+    nama_produk: "Mie goyeng",
+    harga: 3.0,
+    stok: 75,
+  },
+  {
+    kode_barang: "MIE-011",
+    nama_produk: "Mie yebus",
+    harga: 2.0,
+    stok: 75,
+  },
+  {
+    kode_barang: "BHN-012",
+    nama_produk: "Bihun",
+    harga: 10.0,
+    stok: 56,
+  },
+  {
+    kode_barang: "SOU-013",
+    nama_produk: "Soun",
+    harga: 10.0,
+    stok: 52,
+  },
+  {
+    kode_barang: "TLR-014",
+    nama_produk: "Telur",
+    harga: 14.5,
+    stok: 69,
+  },
+  {
+    kode_barang: "MCN-015",
+    nama_produk: "Micin",
+    harga: 12.0,
+    stok: 53,
+  },
+  {
+    kode_barang: "SRP-016",
+    nama_produk: "Sirup ABC",
+    harga: 23.0,
+    stok: 43,
+  },
+  {
+    kode_barang: "KPI-017",
+    nama_produk: "Kopi Hitam",
+    harga: 13.5,
+    stok: 83,
+  },
+  {
+    kode_barang: "PNS-018",
+    nama_produk: "Pensil",
+    harga: 5.0,
+    stok: 48,
+  },
+  {
+    kode_barang: "PLP-019",
+    nama_produk: "Pulpen",
+    harga: 7.0,
+    stok: 46,
+  },
+  {
+    kode_barang: "ROT-020",
+    nama_produk: "Sari roti",
+    harga: 18.0,
+    stok: 9,
+  },
+  {
+    kode_barang: "MNM-021",
+    nama_produk: "Gooday",
+    harga: 7.0,
+    stok: 17,
+  },
+  {
+    kode_barang: "PMN-022",
+    nama_produk: "Fox",
+    harga: 8.5,
+    stok: 11,
+  },
+];
+
+console.log(
+  "==========================================================" +
+    "\n" +
+    "kode barang" +
+    "\t\t\t\t" +
+    "nama produk" +
+    "\t\t\t" +
+    "harga" +
+    "\t\t" +
+    "stok" +
+    "\n" +
+    "============================================================="
+);
+
+console.log(
+  stokJual[0].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[0].nama_produk +
+    "\t\t\t\t" +
+    stokJual[0].harga +
+    "\t\t\t" +
+    stokJual[0].stok
+);
+console.log(
+  stokJual[1].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[1].nama_produk +
+    "\t\t\t" +
+    stokJual[1].harga +
+    "\t\t\t" +
+    stokJual[1].stok
+);
+console.log(
+  stokJual[2].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[2].nama_produk +
+    "\t\t\t" +
+    stokJual[2].harga +
+    "\t\t\t" +
+    stokJual[2].stok
+);
+console.log(
+  stokJual[3].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[3].nama_produk +
+    "\t\t\t\t" +
+    stokJual[3].harga +
+    "\t\t\t" +
+    stokJual[3].stok
+);
+console.log(
+  stokJual[4].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[4].nama_produk +
+    "\t\t\t\t\t" +
+    stokJual[4].harga +
+    "\t\t\t" +
+    stokJual[4].stok
+);
+console.log(
+  stokJual[5].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[5].nama_produk +
+    "\t\t\t" +
+    stokJual[5].harga +
+    "\t\t\t" +
+    stokJual[5].stok
+);
+console.log(
+  stokJual[6].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[6].nama_produk +
+    "\t\t\t\t" +
+    stokJual[6].harga +
+    "\t\t\t" +
+    stokJual[6].stok
+);
+console.log(
+  stokJual[7].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[7].nama_produk +
+    "\t\t\t" +
+    stokJual[7].harga +
+    "\t\t\t" +
+    stokJual[7].stok
+);
+console.log(
+  stokJual[8].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[8].nama_produk +
+    "\t\t" +
+    stokJual[8].harga +
+    "\t\t\t" +
+    stokJual[8].stok
+);
+console.log(
+  stokJual[9].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[9].nama_produk +
+    "\t\t\t" +
+    stokJual[9].harga +
+    "\t\t\t" +
+    stokJual[9].stok
+);
+console.log(
+  stokJual[10].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[10].nama_produk +
+    "\t\t\t" +
+    stokJual[10].harga +
+    "\t\t\t" +
+    stokJual[10].stok
+);
+console.log(
+  stokJual[11].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[11].nama_produk +
+    "\t\t\t\t" +
+    stokJual[11].harga +
+    "\t\t\t" +
+    stokJual[11].stok
+);
+console.log(
+  stokJual[12].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[12].nama_produk +
+    "\t\t\t\t" +
+    stokJual[12].harga +
+    "\t\t\t" +
+    stokJual[12].stok
+);
+console.log(
+  stokJual[13].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[13].nama_produk +
+    "\t\t\t\t" +
+    stokJual[13].harga +
+    "\t\t" +
+    stokJual[13].stok
+);
+console.log(
+  stokJual[14].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[14].nama_produk +
+    "\t\t\t\t" +
+    stokJual[14].harga +
+    "\t\t\t" +
+    stokJual[14].stok
+);
+console.log(
+  stokJual[15].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[15].nama_produk +
+    "\t\t\t" +
+    stokJual[15].harga +
+    "\t\t\t" +
+    stokJual[15].stok
+);
+console.log(
+  stokJual[16].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[16].nama_produk +
+    "\t\t\t" +
+    stokJual[16].harga +
+    "\t\t" +
+    stokJual[16].stok
+);
+console.log(
+  stokJual[17].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[17].nama_produk +
+    "\t\t\t\t" +
+    stokJual[17].harga +
+    "\t\t\t" +
+    stokJual[17].stok
+);
+console.log(
+  stokJual[18].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[18].nama_produk +
+    "\t\t\t\t" +
+    stokJual[18].harga +
+    "\t\t\t" +
+    stokJual[18].stok
+);
+console.log(
+  stokJual[19].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[19].nama_produk +
+    "\t\t\t" +
+    stokJual[19].harga +
+    "\t\t\t" +
+    stokJual[19].stok
+);
+console.log(
+  stokJual[20].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[20].nama_produk +
+    "\t\t\t\t" +
+    stokJual[20].harga +
+    "\t\t\t" +
+    stokJual[20].stok
+);
+console.log(
+  stokJual[21].kode_barang +
+    "\t\t\t\t\t" +
+    stokJual[21].nama_produk +
+    "\t\t\t\t\t" +
+    stokJual[21].harga +
+    "\t\t\t" +
+    stokJual[21].stok
+);
+
+console.log("==========================================================");
